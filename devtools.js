f7eb6142739f283e21ebc7c4dc231ee2add01aef
Pablo Lopez <pablo.lopez@splice.com>
2021-10-25 23:07:24 -0300

    moving requests to devtools

--- devtools.js
+++ devtools.js
@@ -1,8 +1,28 @@
+let flagsBody;
+
 // Create a tab in the devtools area
 chrome.devtools.panels.create(
-  'LD Flags',
-  'logo.png',
-  'panel.html',
-  function (panel) {
-  }
+    'LD Flags',
+    'logo.png',
+    'panel.html',
+    function (panel) {
+        panel.onShown.addListener( extPanelWindow => {
+            console.log('panel shown!');
+            chrome.runtime.sendMessage({
+                flags: flagsBody
+            }, (response) => {});
+
+        })
+    }
 );
+
+
+// esto anda, bien nosotro
+chrome.devtools.network.onRequestFinished.addListener(request => {
+
+    if (request?.request?.url?.startsWith('https://app.launchdarkly.com/sdk/')) {
+        request.getContent((body) => {
+            flagsBody = body;
+        });
+    }
+});
--- src/index.js
+++ src/index.js
@@ -1,28 +1,12 @@
-/**
- * MixpanelTool chrome extension
- *
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- * @classname MixpanelTool
- */
-class MixpanelTool {
+class LaunchDarklyTool {
+  flags;
+  isReady;
   constructor() {
-    this.state = {
-      requests: {},
-      flags: {},
-      selectedRequest: {},
-      count: 0,
-      omitMixpanelProperty: false,
-      record: true,
-    };
     this.bindEvents();
   }
 
   bindEvents() {
     $(() => {
-
       $('.prompt')
         .off('keyup')
         .on('keyup', (s) => {
@@ -32,58 +16,29 @@ class MixpanelTool {
     });
   }
 
+  setFlags(flags){
+    this.flags = flags;
+    this.renderFlags(Object.keys(flags));
+  }
+
+  start(){
+    this.isReady = true;
+    if(this.flags){
+      this.renderFlags(Object.keys(this.flags));
+    }
+  }
+
 
   filterFlags(filter){
-    console.log(filter);
-    if(this.state.flags) {
-      const filteredFlags = Object.keys(this.state.flags).filter( flag => flag.toUpperCase().indexOf(filter.toUpperCase()) > -1  );
+    if(this.flags) {
+      const filteredFlags = Object.keys(this.flags).filter( flag => flag.toUpperCase().indexOf(filter.toUpperCase()) > -1  );
       this.renderFlags(filteredFlags);
     }
-
-
-  }
-
-
-  handleLaunchDarklyRequest(requestObject){
-    if(this.state.record && this.isRequestLaunchDarkly(requestObject)){
-      let properties = {};
-      let base64EncodedData = '';
-      let mixpanelRequest = {};
-      let urlParams = '';
-
-      try{
-        if (requestObject.request.method === 'GET') {
-
-          requestObject.getContent(c => {
-            let flags = JSON.parse(c);
-            if(flags){
-              this.state.flags = flags;
-              this.renderFlags(Object.keys(flags));
-            }
-          });
-
-
-        }
-      } catch (error) {
-        console.error('error', error);
       }
-    }
-  }
-
-  isRequestLaunchDarkly(requestObject){
-    if (requestObject && requestObject.request && requestObject.request.url) {
-      return requestObject.request.url.startsWith('https://app.launchdarkly.com/sdk/');
-    }
-    return false;
-  }
-
-
-
 
   renderFlags(flagKeys){
-
     const nodes = flagKeys.map( (values) => {
-      const val = this.state.flags[values].value;
+      const val = this.flags[values].value;
       const node = $(`<div class="item">
           <i class="small middle aligned icon ${val === true ? 'circle green' : val === false ? 'circle red' : 'flag blue'}"></i>
           <div class="content">${values}</div>
@@ -98,48 +53,31 @@ class MixpanelTool {
   }
 
   renderValues(key){
-    const values = this.state.flags[key];
+    const values = this.flags[key];
     const nodes = Object.keys(values).map( key => {
       return `<div> ${key} : ${values[key]} </div>`;
     });
 
     $('#values').html(nodes);
   }
-
-
-
-  scrollToTop() {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: 'smooth',
-    });
-  }
-
-  manageScrollToTop() {
-    // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function () {
-      scrollFunction();
-    };
-
-    function scrollFunction() {
-      if (
-        document.body.scrollTop > 50 ||
-        document.documentElement.scrollTop > 50
-      ) {
-        $('#scroll-up-btn').show();
-      } else {
-        $('#scroll-up-btn').hide();
-      }
-    }
-  }
 }
 
-// ready function for jQuery
+
+const mixpanelTool = new LaunchDarklyTool();
+
+
+//Handler request from background page
+chrome.extension.onMessage.addListener(function (message, sender) {
+  console.log(message.flags);
+  if(message.flags){
+    mixpanelTool.setFlags(JSON.parse(message.flags));
+    if(mixpanelTool.isReady){
+      mixpanelTool.start();
+    }
+  }
+});
+
+
 $(function () {
-  // create mixpanel tool instance
-  const mixpanelTool = new MixpanelTool();
-  devToolsNetworkListener((request) =>
-    mixpanelTool.handleLaunchDarklyRequest(request)
-  );
+  mixpanelTool.start();
 });
